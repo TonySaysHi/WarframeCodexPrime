73b36b1f61cb2751002780f82b4c16d180d21564
TonySaysHi <tonydang3000@gmail.com>
2018-07-28 20:25:46 -0400

    Damage type and number

---
+++ src/weaponData/Weapon.java
@@ -0,0 +1,14 @@
+package weaponData;
+import java.util.ArrayList;
+
+public class Weapon {
+private String name;
+private ArrayList Damage;
+
+public Weapon(String name, ArrayList damage) {
+	super();
+	this.name = name;
+	Damage = damage;
+}
+
+}
