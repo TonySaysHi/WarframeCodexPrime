3fc9b08fd12837c5aa6b24e8ee2f6d10f5be1b7a
Goruku <41842302+Goruku@users.noreply.github.com>
2018-07-28 20:02:43 -0400

    Status effects and more damage types
    Added the missing element combos and their effect.

--- src/weaponData/DamageType.java
+++ src/weaponData/DamageType.java
@@ -1,5 +1,35 @@
 package weaponData;
 
 public enum DamageType {
-	Slash,Puncture,Impact,Heat,Electric,Cold,Toxin
+	SLASH(StatusEffect.BLEED),
+	PUNCTURE(StatusEffect.DAMAGE_MINUS),
+	IMPACT(StatusEffect.STAGGER),
+	HEAT(StatusEffect.BURN),
+	COLD(StatusEffect.SLOWDOWN),
+	ELECTRIC(StatusEffect.STUN),
+	TOXIN(StatusEffect.POISON),
+	BLAST(StatusEffect.KNOCKDOWN),
+	RADIATION(StatusEffect.FRIENDLY_FIRE),
+	GAS(StatusEffect.TOXIN_CLOUD),
+	MAGNETIC(StatusEffect.SHIELD_MINUS),
+	VIRAL(StatusEffect.HEALTH_MINUS),
+	CORROSIVE(StatusEffect.ARMOR_MINUS),
+	VOID(StatusEffect.BULLET_ATTRACT),
+	TRUE(null);
+	
+	private StatusEffect status;
+	
+	DamageType(StatusEffect status) {
+		this.status = status;
+	}
+
+	public StatusEffect getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEffect status) {
+		this.status = status;
+	}
+	
+	
 }
