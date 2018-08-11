de11adaa1c95062a1a82a02a6661590865151738
TonySaysHi <tonydang3000@gmail.com>
2018-08-11 01:41:38 -0400

    Ability object instance implimented

--- Warframe_Builder/AbilityData/AbilityAttribute.java
+++ Warframe_Builder/AbilityData/AbilityAttribute.java
@@ -1,5 +1,27 @@
 package AbilityData;
 
 public class AbilityAttribute {
+private EffectType type;
+private double number;
 
+public AbilityAttribute(EffectType type, double number) {
+	this.type = type;
+	this.number = number;
+}
+
+public EffectType getType() {
+	return type;
+}
+
+public void setType(EffectType type) {
+	this.type = type;
+}
+
+public double getNumber() {
+	return number;
+}
+
+public void setNumber(double number) {
+	this.number = number;
+}
 }
--- Warframe_Builder/WarframeData/Warframe.java
+++ Warframe_Builder/WarframeData/Warframe.java
@@ -1,5 +1,14 @@
 package WarframeData;
 
 public class Warframe {
+private double health;
+private double shield;
+private double energy;
+private double armor;
+private double sprint_speed;
+private double duration;
+private double efficiency;
+private double range;
+private double strenght;
 
 }
